43a6739128d0280a520eaa74127061a6ccd734ce
stisdol612 <steven.tisdol@gmail.com>
2022-06-10 15:11:21 -0400

    first commit

---
+++ lambdafunc.py
@@ -0,0 +1,15 @@
+import boto3
+
+region = 'us-east-1'
+instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
+ec2 = boto3.client('ec2', region_name=region)
+
+#Used to Stop Instances
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
+
+#Used to Start instances
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
