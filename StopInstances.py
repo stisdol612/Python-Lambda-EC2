fea9bc5c43dfc5e14325951f58c9ff1ceafe9595
stisdol612 <steven.tisdol@gmail.com>
2022-06-19 13:51:17 -0400

    New Commit

--- StopInstances.py
+++ StopInstances.py
@@ -1,7 +1,7 @@
 import boto3
 
 region = 'us-east-1'
-instances = ['i-06fa8e63218becebf', 'i-075545d2dbaf1d0e4']
+instances = ['i-0c7b862d1b69a19dc']
 ec2 = boto3.client('ec2', region_name=region)
 
 #Used to Stop Instances
@@ -9,7 +9,3 @@ def lambda_handler(event, context):
     ec2.stop_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
 
-#Used to Start instances
-def lambda_handler(event, context):
-    ec2.start_instances(InstanceIds=instances)
-    print('started your instances: ' + str(instances))
